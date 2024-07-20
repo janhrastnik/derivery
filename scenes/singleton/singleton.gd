a6c49574c327d27417e429c432426363593515ef
janhrastnik <jan.hrastnik2@gmail.com>
2024-07-21 01:50:19 +0200

    level 2 layout

--- scenes/singleton/singleton.gd
+++ scenes/singleton/singleton.gd
@@ -2,7 +2,7 @@ extends Node
 
 @onready var music_player: AudioStreamPlayer = get_node("AudioStreamPlayer")
 
-var current_level = 1
+var current_level = 0
 
 var player = null
 var delivery_box = null
@@ -27,4 +27,4 @@ func level_complete_event():
 		player.level_complete()
 
 func switch_level():
-	get_tree().change_scene_to_file("res://scenes/levels/level 1/level_{lvl}.tscn".format({"lvl": str(current_level)}))
+	get_tree().change_scene_to_file("res://scenes/levels/level {lvl}/level_{lvl}.tscn".format({"lvl": str(current_level)}))
